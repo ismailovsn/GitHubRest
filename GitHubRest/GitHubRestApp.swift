13ebe3f66f80320684ba6f041a8844242c820597
Said-Nasir Ismailov <ismailovsn12@gmail.com>
2023-12-30 09:54:44 +0900

    Initial Commit

---
+++ GitHubRest/GitHubRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubRestApp.swift
+//  GitHubRest
+//
+//  Created by Саид-Насир Исмаилов on 2023/12/30.
+//
+
+import SwiftUI
+
+@main
+struct GitHubRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
